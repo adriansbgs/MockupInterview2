cb0eabdeed1b06b879307e7153e1fa008ab7b1f4
Adrian Sabagus <adrian@happy5.co>
2026-09-03 21:56:50 +0700

    Fix wrong import of `Singleton`

--- app/src/main/java/com/example/parkee/di/NetworkModule.kt
+++ app/src/main/java/com/example/parkee/di/NetworkModule.kt
@@ -7,7 +7,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Singleton
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
@@ -15,6 +14,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
