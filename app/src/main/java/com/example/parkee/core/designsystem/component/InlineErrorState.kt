60d1288663c81ea5e277a2e360d145d1267325b7
Adrian Sabagus <adrian@happy5.co>
2026-09-03 15:22:26 +0700

    Add padding horizontal 16dp for `inline error` component

--- app/src/main/java/com/example/parkee/core/designsystem/component/InlineErrorState.kt
+++ app/src/main/java/com/example/parkee/core/designsystem/component/InlineErrorState.kt
@@ -2,6 +2,9 @@ package com.example.parkee.core.designsystem.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,11 +18,11 @@ fun InlineErrorState(
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier,
+        modifier = modifier.padding(horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        // TODO: Text(error.toMessage())
-        // TODO: TextButton(onClick = onRetry {Text("Retry")})
+        Text(error.toMessage())
+        TextButton(onClick = onRetry) { Text("Try Again") }
     }
 }
