6dbe53572d3388620f59a58fc0232c45d2506350
Adrian Sabagus <adrian@happy5.co>
2026-09-05 11:25:11 +0700

    Remove `turbine`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,6 @@ dependencies {
 
     //Unit Test
     testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.turbine)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
